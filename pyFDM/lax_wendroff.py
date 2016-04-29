1170298deefac499ef7b258bc66da4c19110dd38
Alexandra K. Diem <A.K.Diem@soton.ac.uk>
2016-04-29 16:03:17 +0100

    less memory usage as not all time step are stored

--- pyFDM/lax_wendroff.py
+++ pyFDM/lax_wendroff.py
@@ -33,6 +33,11 @@ class LaxWendroff(object):
         
         
     @property   
+    def dtr(self):
+        return self._dtr
+        
+        
+    @property   
     def init_cond(self):
         return self._init_cond
         
@@ -40,52 +45,87 @@ class LaxWendroff(object):
     @property   
     def U(self):
         return self._U
+        
+        
+    @U.setter
+    def U(self, value): 
+        self._U = value
+        
+        
+    @property   
+    def ir(self):
+        return self._ir
+        
+        
+    @ir.setter
+    def ir(self, value): 
+        self._ir = value
+        
+        
+    @property   
+    def U0(self):
+        return self._U0
+        
+        
+    @U0.setter
+    def U0(self, value): 
+        self._U0 = value
     
     
-    def __init__(self, nt, nx, dt, dx, init_cond):
-        self._nt = nt
-        self._nx = nx
+    def __init__(self, nt, nx, dt, dx, init_cond, ntr=100):
+        self._nt = int(nt)
+        self._nx = int(nx)
         self._dt = dt
         self._dx = dx
+        self._dtr = nt*dt/ntr
+        self._U = np.zeros((2, ntr, nx))
+        self._ir = 0
         self._init_cond = init_cond
+        self._U0 = np.zeros((2,nx))
         
         
     def initial_conditions(self):
-        N = len(init_cond)
+        N = len(self.init_cond)
         # check that we're dealing with 2 equations
         if N != 2:
             raise ValueError("%d initial conditions supplied, 2 expected."\
                         % (N))
-        U = np.zeros((N, nt, nx))
         for i in range(N):
-            U[i,0,:] = init_cond[i]
-        return U
-        
-        
+            self.U0[i,:] = self.init_cond[i]
+        return self.U0
+     
+     
     def solve(self, inlet_bc, outlet_bc, cfl_condition, F, S, in_args,
                   out_args, cfl_args, F_args, S_args):
-        for i in range(1,nt):
-            U_prev = U[:,i-1,:]
+        U1 = np.zeros((2,self.nx))
+        U0 = self.U0
+        for i in range(1,self.nt):
+            t = i * self.dt
             # inlet boundary condition
-            U[:,i,0] = inlet_bc(U_prev, in_args)
+            U1[:,0] = inlet_bc(U0, t, in_args)
             # outlet boundary condition
-            U[:,i,-1] = outlet_bc(U_prev, out_args)
+            U1[:,-1] = outlet_bc(U0, t, out_args)
             
-            for j in range(1,nx-1):
-                U_prev = U[:,i-1,j-1:j+2]
+            for j in range(1,self.nx-1):
+                U_prev = U0[:,j-1:j+2]
                 if len(U_prev[0,:] == 2):
-                    U_prev = U[:,i-1,j-1:]
+                    U_prev = U0[:,j-1:]
                 F_prev = F(U_prev, F_args)
                 S_prev = S(U_prev, S_args)
-                U[:,i,j] = lax_wendroff(self, U_prev, F_prev, S_prev, F_args,
-                            S_args)
+                U1[:,j] = self.lax_wendroff(U_prev, F_prev, S_prev, F, S,
+                            F_args, S_args)
                 
-                if cfl_condition(U[:,i,j], cfl_args) == False:
+                if cfl_condition(U1[:,j], cfl_args) == False:
                     raise ValueError("CFL condition not fulfilled\nReduce time step size.")
                     sys.exit(1)   
+            if abs(t - self.dtr*i) < self.dt:
+                np.copyto(U[:,self.ir,:], U1)
+                self.ir += 1
+            np.copyto(U0, U1)
+        return self.U
                     
                     
-    def lax_wendroff(self, U_prev, F_prev, S_prev, F_args, S_args):
+    def lax_wendroff(self, U_prev, F_prev, S_prev, F, S, F_args, S_args):
         # u_prev = [U[m-1], U[m], U[m+1]], F_prev, S_prev analogously
         U_np_mp = (U_prev[:,2]+U_prev[:,1])/2 + self.dt/2 *\
                     (-(F_prev[:,2]-F_prev[:,1])/self.dx +\
@@ -97,7 +137,6 @@ class LaxWendroff(object):
         F_np_mm = F(U_np_mm, F_args)
         S_np_mp = S(U_np_mp, S_args)
         S_np_mm = S(U_np_mm, S_args)
-        
         U_np = U_prev[:,1] - self.dt/self.dx * (F_np_mp-F_np_mm) + self.dt/2 *\
                 (S_np_mp+S_np_mm)
         return U_np
