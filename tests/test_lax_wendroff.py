c90c3752046dfb9f10d2f2e0670ba60b8e7617f4
akdiem <A.K.Diem@soton.ac.uk>
2016-10-21 12:46:08 +0100

    unit tests for LaxWendroff

--- tests/test_lax_wendroff.py
+++ tests/test_lax_wendroff.py
@@ -6,54 +6,35 @@ from vampy.artery import *
 import numpy as np
 
 
-def test_init():
+def create_lw():
+    theta = 0.1
+    gamma = 0.1
     nx = 10
-    dx = 0.1
-    lw = LaxWendroff(nx, dx)
+    return LaxWendroff(theta, gamma, nx)
+    
+    
+def F(U, **kwargs):
+    return np.zeros_like(U)
+    
+
+def S(U, **kwargs):
+    return np.zeros_like(U)
+
+
+def test_init():
+    theta = 0.1
+    gamma = 0.1
+    nx = 10
+    lw = LaxWendroff(theta, gamma, nx)
+    assert lw.theta == theta
+    assert lw.gamma == gamma
     assert lw.nx == nx
-    assert lw.dx == dx
-    
-    
-def parameter():
-    R = 1.0
-    lam = 20
-    sigma = 0.5
-    rho = 1.06e6
-    mu = 4.88
-    beta = 22967.4
-    ntr = 100
-    return R, lam, sigma, rho, mu, beta, ntr
     
     
 def test_solve():
-    nx = 10
-    dx = 0.1
-    lw = LaxWendroff(nx, dx)
-    U0 = np.zeros((2,nx))
-    u0 = 0.3
-    U_in = np.array([np.pi, u0])
-    U_out = np.array([np.pi, u0])
-    t = 0.0
-    T = 0.85
-    nt = 100
-    dt = 1e-2
-    R, lam, sigma, rho, mu, beta, ntr = parameter()
-    artery = Artery(R, lam, sigma, rho, mu, beta=beta, ntr=ntr)
-    artery.mesh(nx)
-    artery.initial_conditions(u0)
-    artery.set_time(nt, dt, T)
-    U1 = lw.solve(U0, U_in, U_out, t, T, artery.cfl_condition, artery.F,
-                  artery.S, dt)
-    assert U1.shape == U0.shape
+    lw = create_lw()
+    U0 = np.zeros((2,lw.nx))
+    U_in = U_out = np.array([0,0])
+    U1 = lw.solve(U0, U_in, U_out, F, S)
+    assert (U1 == U0).all()
     
-    
-def test_lax_wendroff():
-    U_prev = np.array([[3.3, 3.4, 3.35], [0.1, 0.3, 0.2]])
-    R, lam, sigma, rho, mu, beta, ntr = parameter()
-    artery = Artery(R, lam, sigma, rho, mu, beta=beta, ntr=ntr)
-    F_prev = artery.F(U_prev)
-    S_prev = artery.S(U_prev)
-    nx = 10
-    dx = 0.1
-    lw = LaxWendroff(nx, dx)
-    U_np = lw.lax_wendroff(U_prev, F_prev, S_prev, artery.F, artery.S, 1e-4)
