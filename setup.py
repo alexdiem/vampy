60e2e342285e6ca2c325e20b7950edb56082259a
akdiem <alexandra.diem@gmail.com>
2017-07-20 07:45:58 +0100

    Corrected setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -20,13 +20,13 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.0',
+    version='1.0.0',
 
-    description="""A python package to solve partial differential equations using finite difference methods (FDM).""",
+    description="""A python package to solve the partial differential equations governing 1D blood flow using finite difference methods (FDM).""",
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/akdiem/VaMpy',
+    url='https://github.com/akdiem/vampy',
 
     # Author details
     author='Alexandra K. Diem',
